80f547cad7e3bee9676f3504b648bc826ae8c256
murej <murej@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2008-11-07 18:26:29 +0000

    first commit of libSRPLcoordTrf

---
+++ libSRPLcoordTrf/srCtrParams.h
@@ -0,0 +1,22 @@
+/*
+ * srBuf.h
+ * A convenience structure to define a srBuffer (amp and phase),
+ * along with number of rows and columns
+ *
+ * @author: James Mure-Dubois
+ * @version: 2008.10.30
+ */
+
+#ifndef SRCTRPARAMS
+#define SRCTRPARAMS
+typedef struct srCtr{
+	int nCols;
+    int nRows;
+    float cX; // optical center X position
+    float cY; // optical center Y position
+	float f; // focal [mm]
+	float pixDX; // pixel pitch X direction
+    float pixDY; // pixel pitch X direction
+	float maxMM; // milimeters corresponding to max ushort phase value
+} SRCTR;
+#endif // 
