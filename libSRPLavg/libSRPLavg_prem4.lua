ccb22d09a79f6fe7b3d4ec18d1833456c8a8beb1
james <james@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-02-09 16:31:14 +0000

    added premake4 files for test
    seems to build, but some API changes are disturbing
    principally relative paths for target paths

---
+++ libSRPLavg/libSRPLavg_prem4.lua
@@ -0,0 +1,121 @@
+--*	NOTES:
+--*		- use the '/' slash for all paths.
+--*****************************************************************************
+
+--******* Initial Setup ************
+--*	Most of the setting are set here.
+--**********************************
+
+-- Set the name of your package.
+project "libSRPLavg4"
+	-- Set this if you want a different name for your target than the projects's name.
+	outName						= "libSRPLavg4"
+	uuid						"17F81D4D-B123-451e-A3B1-733DC3A9FB72"
+	-- Set the files to include/exclude.
+	files						{ "*.cpp", "*.h" }
+	--excludes					{ "" }
+	-- Setup the output directory options.
+	--		Note: Use 'libdir' for "lib" kind only.
+	--bindir					"../lib"
+	--libdir					"../lib"
+	-- Set the defines.
+	defines						{ "_SRPL_API_DLL" }
+	implibprefix 				""
+	targetprefix 				""
+	-- Common setup
+	language					"C++"
+	flags						{ "NoPCH" }
+	--******* GENAERAL SETUP **********
+	--*	Settings that are not dependant
+	--*	on the operating system.
+	--*********************************
+
+-- OPTIONS ---------------------------------------------------------------------
+	-- -- Package options
+--	newoption
+--	{
+--		trigger = "libSRPLavg-static",
+--		description = "Build libSRPLavg as static lib rather than dll"
+--	}
+
+	-- -- libSRPLavg dll
+	if _OPTIONS["libSRPLavg-static"] then
+		kind 					"SharedLib"
+	else
+		kind 					"StaticLib"
+	end
+
+	-- -- Dynamic Runtime
+	if not _OPTIONS["dynamic-runtime"] then
+		flags					{ "StaticRuntime" }
+	end
+
+-- CONFIGURATIONS -------------------------------------------------------------
+--
+	-- -- Unicode
+	configuration "unicode"
+		flags					{ "Unicode" }
+		defines					{ "UNICODE", "_UNICODE" }
+
+	-- -- GCC compilers
+	configuration { "code*", "gmake" }
+		objdir					".obj"
+		buildoptions			{ "-O0" }
+
+	-- -- Visual Studio
+	configuration "vs*"
+		-- Windows and Visual C++ 2005/2008
+		defines					{ "_CRT_SECURE_NO_DEPRECATE" }
+
+	-- Release/Debug
+	-- Set the default targetName if none is specified.
+	if #outName <= 0 then outName = project().name end
+	print( outName )
+	print( project().name )
+	-- Set the targets.
+	configuration "Release"
+		targetname				( outName )
+		targetdir				(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin"))
+		objdir					(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/obj"))
+		implibdir				(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/lib")) 
+		defines					{ "NDEBUG" }
+		flags					{ "OptimizeSpeed" }
+	configuration "Debug"
+		targetname 				( outName.."d" )
+		targetdir				(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin"))
+		objdir					(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/obj"))
+		implibdir				(string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/lib"))
+		defines					{ "DEBUG", "_DEBUG" }
+		flags					{ "Symbols" }
+
+	-- -- Operating Systems specific
+	if configuration "windows" then
+		defines					{ "WIN32", "_WINDOWS" }
+	else
+		excludes				{ "**.rc" }		-- Ignore resource files in Linux.
+		buildoptions			{ "-fPIC" }
+	end
+
+	configuration{"macosx", "codeblocks"}
+		libdirs					(string.format('%s', "/usr/local/lib"))
+		
+	configuration "macosx"
+		targetextension		".dylib"
+		linkoptions			"-dynamiclib"
+
+
+
+--if (OS == "macosx") then
+--  tinsert(package.buildflags, {"dylib"})
+--  tinsert(package.linkoptions, {"-dynamiclib"})
+--  if (target =="cb-gcc") then
+--	tinsert( package.libpaths, 
+--      {
+--      	string.format('%s', "/usr/local/lib")
+--        --string.format('%s%s',os.getenv("WXWIN"), "/build-release")
+--      }
+--	)
+--  end
+--end
+
+
---
+++ libSRPLavg/premake4.lua
@@ -0,0 +1,29 @@
+--*	NOTES:
+--*		- use the '/' slash for all paths.
+--*****************************************************************************
+solution "libSRPLavg4"
+	configurations { "Debug", "Release" }
+
+-- This is for including other Premake scripts.
+dofile( "libSRPLavg_prem4.lua" )
+
+-- Add options here.
+newoption
+{
+	trigger = "dynamic-runtime",
+	description = "Use the dynamicly loadable version of the runtime."
+}
+
+newoption
+{
+	trigger = "unicode",
+	description = "Use the Unicode character set"
+}
+
+
+--project.name = "libSRPLavg"
+--project.guid = "F18FE6C0-5B86-49b1-9F6D-EB2B99C61DFD"
+--project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
+--project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
+--dopackage("libSRPLavg")	
+
