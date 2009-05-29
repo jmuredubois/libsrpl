311663922b86903bdfbc9c75c3f8c03d284966d2
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-05-29 09:21:17 +0000

    added more doxy files

---
+++ trunk/libSRPLcoordTrf/docsLibSRPLcoordTrf.c
@@ -0,0 +1,23 @@
+/*! \mainpage 
+ 
+  \section intro_sec Introduction
+ 
+  The libSRPLCTR library is used for coordinates transformation of range maps delivered by a SwissrangerTM camera.\n
+  The most important functions in the library interface are: 
+ PLCTR_Open(), PLCTR_Close(),PLCTR_coordTrf() .\n
+   \n
+  The class CamSRcoordTrf contains the implementation for all these functions. \n
+  \n
+  \n
+
+ 
+  \section contact_sec  Contact
+  <CENTER>
+   Pattern Recognition Laboratory (PARLAB)  \n
+   Institute of Microtechnology, \n
+   rue A.-L. Breguet,2 \n
+   CH-2000 NEUCHATEL \n
+  </CENTER>
+  For technical info on the library: james.mure-dubois@unine.ch
+
+ */
