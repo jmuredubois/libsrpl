1be018800431a85afdde204f50bea28bb9fdf66f
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-05-29 09:15:08 +0000

    added doxy file for doc generation

---
+++ trunk/libSRPLavg/docs.c
@@ -0,0 +1,35 @@
+/*! \mainpage 
+ 
+  \section intro_sec Introduction
+ 
+  The libSRPLavg library is used for averaging of range maps delivered by a SwissrangerTM camera.\n
+  The most important functions in the library interface are: 
+ PLAVG_Open(), PLAVG_LearnBackgroundInitReset(),PLAVG_GetAvgBuf() .\n
+   \n
+  The complete list of functions includes: \n
+PLAVG_Open()
+PLAVG_Close()
+PLAVG_LearnBackgroundInitReset()
+PLAVG_LearnBackground(()
+PLAVG_GetAvgBuf()
+PLAVG_GetAvgVar()
+PLAVG_GetAvgCnt(()
+PLAVG_SetAvgAlpha()
+PLAVG_GetAvgAlpha()
+  \n
+  The class CamSRavg contains the implementation for all these functions. \n
+  \n
+  Parameters for segmentation are stored in threshold lists from the Thrhld class. \n
+  \n
+
+ 
+  \section contact_sec  Contact
+  <CENTER>
+   Pattern Recognition Laboratory (PARLAB)  \n
+   Institute of Microtechnology, \n
+   rue A.-L. Breguet,2 \n
+   CH-2000 NEUCHATEL \n
+  </CENTER>
+  For technical info on the library: james.mure-dubois@unine.ch
+
+ */
