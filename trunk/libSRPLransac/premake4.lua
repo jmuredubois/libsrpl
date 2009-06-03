daf6e16c025d811235814bd5719983bb01f44bf0
james <james@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-06-03 22:48:53 +0000

    added premake4 files, which produce correct dylibs on mac not done for wxSRparlab

---
+++ trunk/libSRPLransac/premake4.lua
@@ -0,0 +1,28 @@
+--*	NOTES:
+--*		- use the '/' slash for all paths.
+--*****************************************************************************
+solution "libSRPLransac"
+	configurations { "Debug", "Release" }
+
+-- This is for including other Premake scripts.
+dofile( "libSRPLransac_prem4.lua" )
+
+-- Add options here.
+newoption
+{
+	trigger = "dynamic-runtime",
+	description = "Use the dynamicly loadable version of the runtime."
+}
+
+newoption
+{
+	trigger = "unicode",
+	description = "Use the Unicode character set"
+}
+
+--project.name = "libSRPLransac"
+--project.guid = "02097215-5D50-473a-8B0A-ABC766960858"
+--project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
+--project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
+--dopackage("libSRPLransac")	
+
---
+++ trunk/libSRPLscat/premake4.lua
@@ -0,0 +1,27 @@
+--*	NOTES:
+--*		- use the '/' slash for all paths.
+--*****************************************************************************
+solution "libSRPLscat"
+	configurations { "Debug", "Release" }
+
+-- This is for including other Premake scripts.
+dofile( "libSRPLscat_prem4.lua" )
+
+-- Add options here.
+newoption
+{
+	trigger = "dynamic-runtime",
+	description = "Use the dynamicly loadable version of the runtime."
+}
+
+newoption
+{
+	trigger = "unicode",
+	description = "Use the Unicode character set"
+}
+
+--project.name = "libSRPLscat"
+--project.guid = "4ABEECF0-7031-4790-8A05-C7DFE64BD37E"
+--	project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
+--	project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
+--	dopackage("libSRPLscat")	
