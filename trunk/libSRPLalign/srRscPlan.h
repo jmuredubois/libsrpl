6e0dbdce302cb8d3da9ffe220227bcb4c1cd2fb4
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-06-13 12:46:42 +0000

    first commit of class whose job will be to generate alignment matrices

---
+++ trunk/libSRPLalign/srRscPlan.h
@@ -0,0 +1,19 @@
+/*
+ * srRscPlan.h
+ * A convenience structure to define a SR ransac plan 
+ * which has a normal and a list of inliers
+ *
+ * @author: James Mure-Dubois
+ * @version: 2009.05.25
+ */
+
+#ifndef SRRSCPLANHEADER
+#define SRRSCPLANHEADER
+typedef struct rscPlanVar{
+    std::vector<int> inliers;
+	std::vector<int> outliers;
+    double nVec[4];
+	int iter;
+} RSCPLAN;
+#endif // 
+
