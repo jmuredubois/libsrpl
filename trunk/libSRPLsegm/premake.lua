a5abfa8de5ef432b2061a52fa508a6c09086c946
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-11-24 11:28:30 +0000

    added PARLAB EPFL GNU license (v3) + trying out svn keywords

--- trunk/libSRPLsegm/premake.lua
+++
@@ -1,6 +0,0 @@
-project.name = "libSRPLsegm"
-project.guid = "089F867E-40B6-447d-9399-AF8096D2869A"
-project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
-project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
-dopackage("libSRPLsegm")	
-
