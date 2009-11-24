203371ebadbecd35f965c887d53199e1eff55165
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-11-24 11:04:57 +0000

    added PARLAB EPFL GNU license (v3) + trying out svn keywords

--- libSRPLavg/premake.lua
+++
@@ -1,6 +0,0 @@
-project.name = "libSRPLavg"
-project.guid = "F18FE6C0-5B86-49b1-9F6D-EB2B99C61DFD"
-project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
-project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
-dopackage("libSRPLavg")	
-
