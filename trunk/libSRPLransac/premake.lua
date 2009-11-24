35b682cc995e994c1a212e3359e2f3e83788bc0c
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-11-24 11:15:05 +0000

    added PARLAB EPFL GNU license (v3) + trying out svn keywords

--- trunk/libSRPLransac/premake.lua
+++
@@ -1,6 +0,0 @@
-project.name = "libSRPLransac"
-project.guid = "02097215-5D50-473a-8B0A-ABC766960858"
-project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
-project.config["Release"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Release/bin")
-dopackage("libSRPLransac")	
-
--- trunk/libSRPLransac/srRscPlan.h
+++ trunk/libSRPLransac/srRscPlan.h
@@ -3,8 +3,25 @@
  * A convenience structure to define a SR ransac plan 
  * which has a normal and a list of inliers
  *
- * @author: James Mure-Dubois
- * @version: 2009.05.25
+ *    Copyright 2009 James Mure-Dubois, Heinz Hügli and Institute of Microtechnology of EPFL.
+ *      http://parlab.epfl.ch/
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Rev$:
+ * $Author$:
+ * $Date$:
  */
 
 #ifndef SRRSCPLANHEADER
