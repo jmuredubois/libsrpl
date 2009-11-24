43c7291c60c69ca716f16d252eab8fb03f1aa00b
muredubo <muredubo@38107bfa-d90a-11de-b590-7f2b5bfbe1fd>
2009-11-24 11:33:26 +0000

    added PARLAB EPFL GNU license (v3) + trying out svn keywords

--- libSRPLfile/premake.lua
+++ libSRPLfile/premake.lua
@@ -1,3 +1,26 @@
+-- premake default script file to automate building of libSRPLfile
+--  THIS LIBRARY IS NOT COMPLETE YET, AND CURRENTLY NOT NEEDED
+--
+--    Copyright 2009 James Mure-Dubois, Heinz Hügli and Institute of Microtechnology of EPFL.
+--      http://parlab.epfl.ch/
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- $Rev$:
+-- $Author$:
+-- $Date$:
+
 project.name = "libSRPLfile"
 project.guid = "BD6ED18B-4357-4ABE-B885-5531AB137619"
 project.config["Debug"].bindir = string.format('%s%s',os.getenv("JMU_BUILDS"), "/Debug/bin")
